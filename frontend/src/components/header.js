ba3d1bba0f8d5cb27b8daf07f4d0935ce646e13c
Test <test@192-168-1-100.tpgi.com.au>
2021-03-14 17:27:04 +1030

    navbar -> stage 2 - complete

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+
+
 const Header = () => {
   return (
     <header>
       <Navbar bg="primary" variant = 'dark' expand="lg">
-  <Navbar.Brand href="/">Blog Shop</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="/cart">Cart</Nav.Link>
-      <Nav.Link href="/login">Sign In</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
+        <Container>
+          <Navbar.Brand href="/">Blog Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+              <Nav.Link href="/login"><i className='fas fa-user-astronaut'></i> Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   )
 }
