028e054a66707e699cb9a733bab3612e722a9dcb
Test <test@192-168-1-100.tpgi.com.au>
2021-03-14 17:51:46 +1030

    adding product images and product object file

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Apple Airpods With Standard Charching Case',
+    image: '/images/airpods.png',
+    description:
+      'The new AirPods combine intelligent design with breakthrough technology and crystal-clear sound. Powered by the new Apple H1 headphone chip, AirPods now feature hands-free access to Siri using just your voice. And up to three hours of talk time on a single charge.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 249.00,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'iPhone 12 Pro 256GB Memory',
+    image: '/images/phone.png',
+    description:
+      'iPhone 12 Pro. 5G to download huge files on the go and stream HDR video.¹ Beautifully bright 6.1-inch Super Retina XDR display.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 1869.00,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Instax Mini 90 Classic Instant Camera - BLACK',
+    image: '/images/instax.png',
+    description:
+      'Stylish yet classic good looks, combined with the latest feature-rich image tech. Meet the instax mini 90 - designed to keep even the snap-happiest photo lover in seventh heaven.',
+    brand: 'Fuji',
+    category: 'Electronics',
+    price: 219.00,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Apple iMac 27-INCH Retina 5K display',
+    image: '/images/desktop.png',
+    description:
+      'iMac performance is at an all-time high with 10th-generation Intel Core processors, powerful AMD Radeon Pro 5000 series graphics, ultrafast SSD storage and Thunderbolt 3 (USB-C) connectivity.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 3549.00,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'MacBook Pro 16-Inch 2.3GHZ/16GB/1TB Silver',
+    image: '/images/laptop.png',
+    description:
+      'Expand your view of everything on MacBook Pro thanks to a larger 16-inch Retina display with sharper pixel resolution and support for millions of colours.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 4399.00,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'TDE 16-Inch Laptop Case',
+    image: '/images/laptop-case.png',
+    description:
+      'Our classic laptop case perfectly fits a 13 inch laptop. With two zippers to keep your laptop safe and sound, and a black fabric lining, it is a staple item that can be carried on its own, or placed into a leather tote or carry-bag as you go about your day.',
+    brand: 'The Daily Edited',
+    category: 'Electronics',
+    price: 149.95,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
