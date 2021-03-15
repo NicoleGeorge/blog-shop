8a22477bfee72722b39b62241160205934c459b0
Nicole George <hello@nicolegeorge.me>
2021-03-15 21:25:18 +1030

    adding product price, description & starting sidebar

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -28,6 +28,35 @@ const ProductScreen = ({ match }) => {
               />
             </ListGroup.Item>
           </ListGroup>
+          <ListGroup.Item>Product: ${product.price}</ListGroup.Item>
+          <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Sold Out'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button className="btn-block" type="button">
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
         </Col>
       </Row>
     </>
