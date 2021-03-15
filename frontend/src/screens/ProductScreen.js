445c846bae114f4cb6e7d0fba48224ab471c7bc8
Nicole George <hello@nicolegeorge.me>
2021-03-15 21:27:25 +1030

    add to cart button disabled when item is out of stock function

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -51,7 +51,11 @@ const ProductScreen = ({ match }) => {
                 </Row>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Button className="btn-block" type="button">
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={(product.countInStock === 0)}
+                >
                   Add To Cart
                 </Button>
               </ListGroup.Item>
