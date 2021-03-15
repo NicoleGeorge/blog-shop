8ac574b7345245948fc70f85cb8b7cb59de7ad60
Nicole George <hello@nicolegeorge.me>
2021-03-15 19:27:23 +1030

    adding product image, title and ratings to indiv. product page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -7,7 +7,31 @@ import products from '../products';
 const ProductScreen = ({ match }) => {
   const product = products.find((p) => p._id === match.params.id);
 
-  return <div>{product.name}</div>;
+  return (
+    <>
+      <Link className="btn btn-primary my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid></Image>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>{product.name}</h2>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ProductScreen;
