f27b56bb00ab6e539d3005f2ab4982db444d2ce1
Nicole George <hello@nicolegeorge.me>
2021-07-04 17:59:07 +0930

    setting up backend server

---
+++ backend/data/products.js
@@ -0,0 +1,95 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Instax Mini 90 Classic Instant Camera - BLACK',
+    image: '/images/instax.png',
+    description:
+      'Stylish yet classic good looks, combined with the latest feature-rich image tech. Meet the instax mini 90 - designed to keep even the snap-happiest photo lover in seventh heaven.',
+    brand: 'Fuji',
+    category: 'Electronics',
+    price: 219.0,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Apple iMac 27" Retina 5K display',
+    image: '/images/desktop.png',
+    description:
+      'iMac performance is at an all-time high with 10th-generation Intel Core processors, powerful AMD Radeon Pro 5000 series graphics, ultrafast SSD storage and Thunderbolt 3 (USB-C) connectivity.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 3549.0,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '3',
+    name: 'iPhone 12 Pro 256GB Memory Silver',
+    image: '/images/phone.png',
+    description:
+      'iPhone 12 Pro. 5G to download huge files on the go and stream HDR video.¹ Beautifully bright 6.1-inch Super Retina XDR display.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 1869.0,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '4',
+    name: 'MacBook Pro 16-Inch Silver',
+    image: '/images/laptop.png',
+    description:
+      'Expand your view of everything on MacBook Pro thanks to a larger 16-inch Retina display with sharper pixel resolution and support for millions of colours.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 4399.0,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '5',
+    name: 'TDE 16-Inch Laptop Case',
+    image: '/images/laptop-case.png',
+    description:
+      'Our classic laptop case perfectly fits a 13 inch laptop. With two zippers to keep your laptop safe and sound, and a black fabric lining, it is a staple item that can be carried on its own, or placed into a leather tote or carry-bag as you go about your day.',
+    brand: 'The Daily Edited',
+    category: 'Accessories',
+    price: 149.00,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: '6',
+    name: 'Apple Airpods w/ Charging Case',
+    image: '/images/airpods.png',
+    description:
+      'The new AirPods combine intelligent design with breakthrough technology and crystal-clear sound. Powered by the new Apple H1 headphone chip, AirPods now feature hands-free access to Siri using just your voice. And up to three hours of talk time on a single charge.',
+    brand: 'Apple',
+    category: 'Accessories',
+    price: 249.0,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '7',
+    name: 'Apple iPad 10.2 Inch White',
+    image: '/images/tablet.png',
+    description:
+      'iPad combines a beautiful 10.2-inch Retina display and tremendous capability with unmatched versatility and ease of use. And with the powerful A12 Bionic chip, support for Apple Pencil² And the amazing new things you can do with iPadOS 14, there is even more to love about iPad.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 699.0,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 12,
+  },
+];
+
+module.exports = products;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -9,11 +9,11 @@ import HomeScreen from './screens/HomeScreen'
 function App() {
   return (
     <Router>
-      <Header/>
+      <Header />
       <main className='py-3'>
         <Container>
-                <Route path='/' component={HomeScreen} exact/>
-                <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
